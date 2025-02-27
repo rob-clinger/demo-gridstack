614239a19caf35c3933ab55854bcc0d30025d21d
Rob Clinger <Robert7595@gmail.com>
2025-02-27 15:04:32 -0500

    gridstack-demo initial commit

---
+++ src/main/java/com/example/demo_gridstack/DemoGridstackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_gridstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGridstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGridstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_gridstack/DemoGridstackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_gridstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGridstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
